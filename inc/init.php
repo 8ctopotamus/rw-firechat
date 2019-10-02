684f813f797ba6d23c2cf7d7ad52556233da52c5
8ctopotamus <joshnaylor88@gmail.com>
2019-10-02 12:24:55 -0500

    Frontend chat, start, chatUI, firebase start

--- inc/init.php
+++ inc/init.php
@@ -9,7 +9,7 @@ define('FIREBASE_CONFIG', array(
   'projectId' => getenv('FIREBASE_PROJECT_ID'),
   'storageBucket' => getenv('FIREBASE_STORAGE_BUCKET'),
   'messagingSenderId' => getenv('FIREBASE_MESSAGING_SENDER_ID'),
-  'appId' => getenv('FIREBASE_APP_ID')
+  'appId' => getenv('FIREBASE_APP_ID'),
 ));
 
 
@@ -59,7 +59,29 @@ add_action('wp_enqueue_scripts', 'rw_support_chat_assets');
  * Frontend Chat UI
  */
 function rw_support_chat_footer() {
-  echo '<div id="rw-chat-fab"><span class="dashicons dashicons-format-chat"></span></div>';
-  echo '<div id="rw-chat-ui"><div class="rw-chat-transcript"></div><div><input type="text" /></div></div>';
+  // echo '<div id="rw-chat-fab"><span class="dashicons dashicons-format-chat"></span></div>';
+  echo '<div id="rw-chat-widget">
+    <form id="start-chat">
+      Enter your name and email to start chatting!
+      <input name="name" type="text" placeholder="Name" />
+      <input name="email" type="email" placeholder="Email" />
+      <button type="submit">Start chat</button>
+    </form>
+
+    <div id="rw-chat-ui">
+      <div id="rw-chat-transcript">
+        <div class="chat-bubble">
+          <img src="https://realwealthmarketing.com/wp-content/uploads/2017/05/jade-paczelt.jpg" alt="Help" class="chat-bubble-avatar" />
+          <div>
+            <span class="chat-bubble-name">Jade</span>
+            <span class="chat-bubble-text">Hi there! How can I help?</span>
+          </div>
+        </div>
+      </div>
+      <form id="chat-form">
+        <input type="text" class="form-control" placeholder="Your message..." />
+      </form>
+    </div>
+  </div>';
 }
 add_action('wp_footer', 'rw_support_chat_footer');
--- js/frontend-chat.js
+++ js/frontend-chat.js
@@ -1,13 +1,56 @@
 (function() {
   const { firebaseConfig } = wp_data;
-  const fab = document.getElementById('rw-chat-fab');
-  const chatUI = document.getElementById('rw-chat-ui');
-  
-  const toggleUI = e => {
-    e.target.style.display = 'none';
-    chatUI.style.display = 'block';
-  };
+  const chatWidget = document.getElementById('rw-chat-widget');
+  const startChatForm = document.getElementById('start-chat');
 
-  firebase.initializeApp(firebaseConfig);
-  fab.addEventListener('click', toggleUI);
+  const chatUI = chatWidget.querySelectorAll('#rw-chat-ui')[0];
+  const transcript = chatUI.querySelectorAll('#rw-chat-transcript')[0];
+  const form = chatUI.querySelectorAll('#chat-form')[0];
+  const input = form.querySelectorAll('input')[0];
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = app.firestore();
+
+  let channelID;
+
+  function cloneChatBubble() {
+    const clone = chatWidget.querySelectorAll('.chat-bubble')[0].cloneNode(true);
+    clone.querySelectorAll('.chat-bubble-avatar')[0].src = 'https://realwealthmarketing.com/wp-content/uploads/2017/11/sample-headshot-placeholder.jpg';
+    clone.querySelectorAll('.chat-bubble-name')[0].innerText = 'Guest';
+    return clone;
+  }
+
+  function handleChatFormSubmit(e) {
+    e.preventDefault();
+    const clone = cloneChatBubble();
+    clone.classList.add('guest');
+    clone.querySelectorAll('.chat-bubble-text')[0].innerText = input.value;
+    transcript.appendChild(clone);
+    console.log(channelID)
+    input.value = '';
+  }
+
+  function startChat(e) {
+    e.preventDefault();
+    const data = {
+      name: e.target.querySelectorAll('input[type="text"]')[0].value,
+      value: e.target.querySelectorAll('input[type="email"]')[0].value,
+    };
+    db.collection('channels')
+      .add(data)
+      .then(docRef => channelID = docRef.id)
+      .catch(err => console.log(err));
+    startChatForm.style.display = 'none';
+    chatUI.style.display = 'block';
+  }
+
+  // db.collection("channels")
+  //   .onSnapshot(function(querySnapshot) {
+  //     querySnapshot.forEach(function(doc) {
+  //       console.log(doc.id, doc.data())
+  //     });
+  //   });
+
+  form.addEventListener('submit', handleChatFormSubmit);
+  startChatForm.addEventListener('submit', startChat);
 })();
